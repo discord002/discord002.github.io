040f4f398a83d78a8d2c8902ad16b5ee2d57e2eb
deni999ialg <denbec@rsplus-mayen.de>
2025-12-09 09:00:11 +0100

    Update Community.bot.js

--- media/botshelper/Community.bot.js
+++ media/botshelper/Community.bot.js
@@ -1,1 +1,10 @@
-
+const firebaseConfig = {
+      apiKey: "AIzaSyASJwP_n6DcvfACpEla9L8qpMApp1f5_l4",
+      authDomain: "discord002-e78f0.firebaseapp.com",
+      databaseURL: "https://discord002-e78f0-default-rtdb.firebaseio.com",
+      projectId: "discord002-e78f0",
+      storageBucket: "discord002-e78f0.firebasestorage.app",
+      messagingSenderId: "232259988836",
+      appId: "1:232259988836:web:9ef0dcf4af872ef3965e71",
+      measurementId: "G-XEX6W5FSCT"
+    };
